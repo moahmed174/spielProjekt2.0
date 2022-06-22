29d83060dd0259a58e6269d3205dcce5441f39ee
mihai <mihaibalint982@gmail.com>
2022-06-22 16:05:04 +0200

    Neu: - Class: BBImpl (Klasse für das Balanceboard) - Main: connectingBalanceBoard() (Methode im main um das Board zu verbinden)

--- src/pack1/Main.java
+++ src/pack1/Main.java
@@ -1,5 +1,8 @@
 package pack1;
 
+import wiiremotej.BalanceBoard;
+import wiiremotej.WiiRemoteJ;
+
 public class Main {
     public static void main(String[] args) {
         new Gui();
@@ -14,5 +17,37 @@ public class Main {
         new SchneeballCollision();
         new CollectableCollision();
         new Lost();
+        //connectingBalanceBoard();
+
+    }
+
+    public static void connectingBalanceBoard(){
+        //basic console logging options...
+        WiiRemoteJ.setConsoleLoggingAll();
+        //WiiRemoteJ.setConsoleLoggingOff();
+
+        try
+        {
+            //Find and connect to a Balance Board
+            BalanceBoard board = null;
+
+            while (board == null) {
+                try {
+                    board = WiiRemoteJ.findBalanceBoard();
+                }
+                catch(Exception e) {
+                    board = null;
+                    e.printStackTrace();
+                    System.out.println("Failed to connect board. Trying again.");
+                }
+            }
+
+            board.addBalanceBoardListener(new BBImpl(board));
+            board.setLEDIlluminated(true);
+
+            final BalanceBoard boardF = board;
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable(){public void run(){boardF.disconnect();}}));
+        }
+        catch(Exception e){e.printStackTrace();}
     }
 }
